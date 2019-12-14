83f52ae12f7aad62cafc56cf4982934adee9164c
Daniel Gil <danielgilucsd@gmail.com>
2019-12-14 08:34:36 -0800

    created employe classes

---
+++ classes/Employe.js
@@ -0,0 +1,23 @@
+class Employe{
+    constructor(name,id,email){
+        this.name = name;
+        this.id = id;
+        this.email = email
+    }
+    getname(){
+return this.name 
+
+    }
+    getid(){
+        return this.id
+        
+            }
+            getemail(){
+                return this.email
+                       
+  
+            }
+}
+
+module.exports = Employe
+
